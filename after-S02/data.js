1cdd91577c4729341cdcad34b64153f1b7a83f27
Sepide Kia <70714630+sepkey@users.noreply.github.com>
2023-02-04 14:09:11 +0330

    CSS of FMH app finished

---
+++ after-S02/data.js
@@ -0,0 +1,48 @@
+const CATEGORIES = [
+  { name: "reactjs", color: "#61dafc" },
+  { name: "redux", color: "#7749bd" },
+  { name: "react-query", color: "#ff4154" },
+  { name: "nextjs", color: "#000" },
+  { name: "typescript", color: "#3178c6" },
+  { name: "javascript", color: "#f7e018" },
+  { name: "graphql", color: "#e632ad" },
+  { name: "nodejs", color: "#68a063" },
+];
+
+const initialFacts = [
+  {
+    id: 1,
+    text: "React is being developed by Meta (formerly facebook)",
+    source: "https://opensource.fb.com/",
+    category: "reactjs",
+    votesInteresting: 24,
+    votesMindblowing: 9,
+    votesFalse: 4,
+    createdIn: 2021,
+  },
+  {
+    id: 2,
+    text: "Millennial dads spend 3 times as much time with their kids than their fathers spent with them. In 1982, 43% of fathers had never changed a diaper. Today, that number is down to 3%",
+    source:
+      "https://www.mother.ly/parenting/millennial-dads-spend-more-time-with-their-kids",
+    category: "nextjs",
+    votesInteresting: 11,
+    votesMindblowing: 2,
+    votesFalse: 0,
+    createdIn: 2019,
+  },
+  {
+    id: 3,
+    text: "Lisbon is the capital of Portugal",
+    source: "https://en.wikipedia.org/wiki/Lisbon",
+    category: "typescript",
+    votesInteresting: 8,
+    votesMindblowing: 3,
+    votesFalse: 1,
+    createdIn: 2015,
+  },
+];
+
+// LINK TO APP SAMPLE DATA: https://docs.google.com/spreadsheets/d/1eeldcA_OwP4DHYEvjG0kDe0cRys-cDPhc_E9P9G1e3I/edit#gid=0
+
+// 👍 🤯 ⛔️
